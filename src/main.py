682cae6704b8dead854883d0fc36d7d065be001a
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2025-01-24 10:03:49 -0300

    feature: coletando página
    *falta coletar os livros na página

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import time
 from datetime import datetime
 from pathlib import Path
 from pprint import pprint
@@ -12,17 +13,29 @@ DATA_DIR = (Path() / "data").resolve()
 def main():
     motw = MemoryOfTheWorldAPI()
     current_date = datetime.now()
-    for i in range(1, 2):
-        page_content = motw.get_page_data(i)
+    for page in range(1, 2):
+        page_content = motw.get_page_data(page)
         page_content_df = pd.DataFrame(
-            [{"page": i, "response": page_content, "timestamp": current_date}]
+            [
+                {
+                    "page": page,
+                    "response": page_content,
+                    "timestamp": current_date,
+                }
+            ]
         )
         save_csv(page_content_df, DATA_DIR / "response.csv")
 
-        page_items = page_content["_items"]
-        pprint(
-            [transform_data(item) for item in page_items][0]
-        )
+        page_items = [
+            transform_data(data=item, page=page)
+            for item in page_content["_items"]
+        ]
+        page_items_df = pd.DataFrame.from_records(page_items)
+        save_csv(page_items_df, DATA_DIR / "items.csv")
+
+        pprint(motw.get_book_details(page_items[0]["id"]))
+
+        time.sleep(1.5)
 
 
 if __name__ == "__main__":
--- src/motw.py
+++ src/motw.py
@@ -2,22 +2,27 @@ from pathlib import Path
 
 import httpx
 import pandas as pd
-from urllib.parse import quote_plus
 
 
 class MemoryOfTheWorldAPI:
     def __init__(self):
-        self.url = "https://books.memoryoftheworld.org/books"
         self.last_page = int(
             (
-                httpx.get(f"{self.url}?page=1")
+                httpx.get("https://books.memoryoftheworld.org/books?page=1")
                 .json()["_links"]["last"]["href"]
                 .split("=")[-1]
             )
         )
 
     def get_page_data(self, page: int):
-        return httpx.get(f"{self.url}?page={page}").json()
+        return httpx.get(
+            f"https://books.memoryoftheworld.org/books?page={page}"
+        ).json()
+
+    def get_book_details(self, book_id: str):
+        return httpx.get(
+            f"https://library.memoryoftheworld.org/book/{book_id}"
+        ).json()
 
 
 def save_csv(df: pd.DataFrame, path: Path):
@@ -27,15 +32,20 @@ def save_csv(df: pd.DataFrame, path: Path):
         df.to_csv(path_or_buf=path, index=False, header=True, mode="w")
 
 
-def transform_data(data: dict):
+def transform_page_data(data: dict, page: int):
     def transform_format(fmt: dict):
         fmt["url"] = (
             f"{data['library_url']}{fmt.pop('dir_path')}{fmt.pop('file_name')}"
         )
         return fmt
 
+    data["page"] = page
     data["id"] = data.pop("_id")
     data["library_url"] = f'https:{data["library_url"]}'
     data["cover_url"] = f"{data['library_url']}{data['cover_url']}"
     data["formats"] = [transform_format(fmt) for fmt in data["formats"]]
+    return data
+
+
+def transform_book_data(data: dict):
     return data

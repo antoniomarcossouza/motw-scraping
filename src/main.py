d4998ee162e080838803a2ea7067b6419ab11f24
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2025-01-25 15:01:19 -0300

    feature: salvando JSONs

--- src/main.py
+++ src/main.py
@@ -1,13 +1,8 @@
-from datetime import datetime
+import json
 from pathlib import Path
-
-import pandas as pd
 
 from motw import (
     MemoryOfTheWorldAPI,
-    save_csv,
-    transform_book_data,
-    transform_page_data,
 )
 
 DATA_DIR = (Path() / "data").resolve()
@@ -16,33 +11,24 @@ DATA_DIR.mkdir(exist_ok=True, parents=True)
 
 def main():
     motw = MemoryOfTheWorldAPI()
-    current_date = datetime.now()
     for page in range(1, 2):
+        pages_dir = DATA_DIR / "original" / "pages"
+        pages_dir.mkdir(exist_ok=True, parents=True)
+        page_file = pages_dir / f"page_{page}.json"
+
+        if page_file.is_file():
+            return
+
         page_content = motw.get_page_data(page)
-        page_content_df = pd.DataFrame(
-            [
-                {
-                    "page": page,
-                    "response": page_content,
-                    "timestamp": current_date,
-                }
-            ]
-        )
-        save_csv(page_content_df, DATA_DIR / "response.csv")
+        page_file.write_text(json.dumps(page_content, indent=4))
 
-        page_items = [
-            transform_page_data(data=item, page=page)
-            for item in page_content["_items"]
-        ]
-        page_items_df = pd.DataFrame.from_records(page_items)
-        save_csv(page_items_df, DATA_DIR / "items.csv")
-
-        books = [
-            transform_book_data(motw.get_book_details(book["id"]))
-            for book in page_items
-        ]
-        books_df = pd.DataFrame.from_records(books)
-        save_csv(books_df, DATA_DIR / "books.csv")
+        book_ids = {item["_id"] for item in page_content["_items"]}
+        books_dir = DATA_DIR / "original" / "books"
+        books_dir.mkdir(exist_ok=True, parents=True)
+        for book_id in book_ids:
+            book_file = books_dir / f"{book_id}.json"
+            book_content = motw.get_book_details(book_id)
+            book_file.write_text(json.dumps(book_content, indent=4))
 
         print(f"Page {page} done.")
 
--- src/motw.py
+++ src/motw.py
@@ -1,7 +1,5 @@
-from pathlib import Path
 
 import httpx
-import pandas as pd
 
 
 class MemoryOfTheWorldAPI:
@@ -23,32 +21,3 @@ class MemoryOfTheWorldAPI:
         return httpx.get(
             f"https://library.memoryoftheworld.org/book/{book_id}"
         ).json()
-
-
-def save_csv(df: pd.DataFrame, path: Path):
-    if path.is_file():
-        df.to_csv(path_or_buf=path, index=False, header=False, mode="a")
-    else:
-        df.to_csv(path_or_buf=path, index=False, header=True, mode="w")
-
-
-def transform_page_data(data: dict, page: int):
-    def transform_format(fmt: dict):
-        fmt["url"] = (
-            f"{data['library_url']}{fmt.pop('dir_path')}{fmt.pop('file_name')}"
-        )
-        return fmt
-
-    data["page"] = page
-    data["id"] = data.pop("_id")
-    data["library_url"] = f'https:{data["library_url"]}'
-    data["cover_url"] = f"{data['library_url']}{data['cover_url']}"
-    data["formats"] = [transform_format(fmt) for fmt in data["formats"]]
-    return data
-
-
-def transform_book_data(data: dict):
-    data["id"] = data.pop("_id")
-    cleaned_abstract = data["abstract"].replace("\n", "  ")
-    data["abstract"] = cleaned_abstract
-    return data

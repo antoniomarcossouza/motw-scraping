d2bcd6b0731f9344a6ce7129692817a251214346
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2025-01-26 17:03:06 -0300

    fix: trocando return por continue

--- src/main.py
+++ src/main.py
@@ -11,13 +11,13 @@ DATA_DIR.mkdir(exist_ok=True, parents=True)
 
 def main():
     motw = MemoryOfTheWorldAPI()
-    for page in range(1, 2):
+    for page in range(1, motw.last_page + 1):
         pages_dir = DATA_DIR / "original" / "pages"
         pages_dir.mkdir(exist_ok=True, parents=True)
         page_file = pages_dir / f"page_{page}.json"
 
         if page_file.is_file():
-            return
+            continue
 
         page_content = motw.get_page_data(page)
         page_file.write_text(json.dumps(page_content, indent=4))
